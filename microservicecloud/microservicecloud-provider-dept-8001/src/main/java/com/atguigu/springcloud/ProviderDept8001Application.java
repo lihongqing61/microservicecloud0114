51f66e9da1f4a805857c8423451890fa6d3ff15b
lihongqing61@qq.com <lihongqing61@qq.com>
2019-01-15 16:16:09 +0800

    eureka开发

--- microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/ProviderDept8001Application.java
+++ microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/ProviderDept8001Application.java
@@ -3,9 +3,11 @@ package com.atguigu.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.atguigu.springcloud.dao")
+@EnableEurekaClient             //本服务启动后会自动注册进eureka服务中
 public class ProviderDept8001Application {
 
     public static void main(String[] args) {
